c0cb75b19a25b174bbd229820d7e3fea14f94578
vladlenkiev <mankovskij@yandex.ru>
2017-01-13 10:08:53 +0200

    Done

--- src/test/java/LoginControllerTests.java
+++ src/test/java/LoginControllerTests.java
@@ -1,5 +1,3 @@
-package cloud.molddata.parser.cv.testCases;
-
 import org.junit.Test;
 
 //import static org.junit.Assert.assertEquals;
--- src/test/java/SearcherRegByPhTest.java
+++ src/test/java/SearcherRegByPhTest.java
@@ -1,14 +1,31 @@
-package cloud.molddata.parser.cv.testCases;
-
 import cloud.molddata.parser.cv.parser.SeacrherRegByPh;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class SearcherRegByPhTest {
+/**
+ * The user enters text into a search box. This class is used
+ * to parse that text into specific search terms (or tokens).
+ * It eliminates common words, and allows for the quoting of text, using
+ * double quotes.
+ * JDK 7+.
+ */
+public final class SearcherRegByPhTest {
     @Test
     public void testFindFileCountryCode(){
         SeacrherRegByPh seacrherRegByPhTest = new SeacrherRegByPh();
 
+        String result0 = seacrherRegByPhTest.findFileCountryCode("+38050");
+        String result = seacrherRegByPhTest.findFileCountryCode("+38050");
+
+        assertEquals("resources/location/phone-codes.json", result);
+
+    }
+
+    @Test
+    public void testFindFileCountryCode0(){
+        SeacrherRegByPh seacrherRegByPhTest = new SeacrherRegByPh();
+
+        String result0 = seacrherRegByPhTest.findFileCountryCode("+38050");
         String result = seacrherRegByPhTest.findFileCountryCode("+38050");
 
         assertEquals("resources/location/phone-codes.json", result);
